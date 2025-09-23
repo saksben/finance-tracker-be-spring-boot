bff3ef7a0db412e4b5d1561517ebce48291b52c6
Benjamin Saks <saksben2@gmail.com>
2025-09-22 19:02:16 -0700

    add CORS

--- backend/backend/src/main/java/com/finance_tracker/backend/BackendApplication.java
+++ backend/backend/src/main/java/com/finance_tracker/backend/BackendApplication.java
@@ -2,6 +2,9 @@ package com.finance_tracker.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -9,5 +12,24 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
+
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:3000",
+                                "http://localhost:3001",
+                                "http://localhost:3002",
+                                "http://localhost:3003"
+                        )
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+                        // .allowCredentials(true);
+            }
+        };
+    }
 
 }
